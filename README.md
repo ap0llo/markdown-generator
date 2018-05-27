56fbabe10e96e5515140ee4171c2e58757c0a004
Andreas Gruenwald <andreasgruenwald@live.de>
2018-05-27 10:46:36 +0200

    Add Set-Version script

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# markdown-generator
+# MarkdownGenerator
+
+## Usage
+
+*TODO*
+
+## Building from source
+
+*TODO*
+
+## Versioning and Branching
+
+The version of the library is automatically derived from git and the information in `version.json` using
+[Nerdbank.GitVersioning](https://github.com/AArnott/Nerdbank.GitVersioning):
+
+- The master branch  always contains the latest version. Packages produced from master are always
+  marked as pre-relase versions (using the `-prerelease` suffix).
+- Stable versions are built from release branches. Build from release branches will have no `-prerelease` suffix
+- Builds from any other branch will have both the `-prerelease` and the git commit hash included
+  in the version string
+
+The `Set-Version.ps1` script can be used to create a new release branch and update the version on *master*. 
+The script will:
+
+- read `version.json` to determine the current version of the branch (typically *master*)
+- create a new release branch for version, e.g. *release/v1.0*
+- Remove the pre-release tag from `version.json` on the release branch
+- Increment the version on *master* so builds from that branch will produce pre-release packages
+  for the next version. The next version is either the next major or minor version,
+  depending on the parameters passed to `Set-Version.ps1` (either `-Major` or `-Minor`)
---
+++ Set-Version.ps1
@@ -0,0 +1,83 @@
+param(
+    [switch]$Major,
+    [switch]$Minor
+)
+$versionJsonFilePath = Join-Path $PSScriptRoot "version.json"
+$preReleaseTag = "-prerelease"
+
+function Start-Git($command) {
+    Invoke-Expression "git $command"
+    if($LASTEXITCODE -ne 0) {
+        throw "Command 'git $command' failed (exit code $LASTEXITCODE)"
+    }
+}
+
+if(-not ($Major -or $Minor)) {
+    throw "Use either the -Major or -Minor switch to increment the version"
+}
+if($Major -and $Minor) {
+    throw "The switches -Major or -Minor cannot be used together"
+}
+
+if(-not (Test-Path $versionJsonFilePath)) {
+    throw "version.json not found (expected at '$versionJsonFilePath')"
+}
+
+$currentBranchName = Start-Git "rev-parse --abbrev-ref HEAD"
+
+if($currentBranchName -eq $null) {
+    throw "Failed to dermine current branch"
+}
+
+# Read version.json to determine version to be release
+try {
+    $versionInfo = ConvertFrom-Json (Get-Content -Raw -Path $versionJsonFilePath)
+    $currentVersion = $versionInfo.version
+    $releaseVersion = [version]($versionInfo.version.Replace($preReleaseTag, ""))
+} catch {
+    throw
+}
+
+# Determine new version for current branch
+if($Major) {
+    $newVersion = "$($releaseVersion.Major + 1).0$preReleaseTag"
+} elseif($Minor) {
+    $newVersion = "$($releaseVersion.Major).$($releaseVersion.Minor + 1)$preReleaseTag"
+}
+
+Write-Host "Current version is '$currentVersion'"
+Write-Host "Version to be released is '$releaseVersion'"
+Write-Host "Version for current branch '$currentBranchName' will be set to '$newVersion'"
+
+# Prompt for user confirmation
+$choice = ""
+while ($choice -notmatch "[y|n]"){
+    $choice = Read-Host "Do you want to continue? (Y/N)"
+}
+if ($choice -eq "n"){
+    exit 0
+}
+    
+
+# Create release branch for current version
+$releaseBranchName = "release/v$releaseVersion"
+
+Write-Host "Creating release branch '$releaseBranchName'"
+Start-Git "checkout -b $releaseBranchName"
+
+Write-Host "Updating version.json on release branch"
+$versionInfo.version = "$($releaseVersion.Major).$($releaseVersion.Minor)"
+$versionInfo | ConvertTo-Json | Out-File $versionJsonFilePath -Encoding utf8
+
+Start-Git "add `"$versionJsonFilePath`""
+Start-Git "commit -m `"Create release branch v$releaseVersion`""
+
+# Switch current branch to new version
+Write-Host "Updating version.json on branch $currentBranchName"
+Start-Git "checkout $currentBranchName"
+$versionInfo.version = $newVersion
+$versionInfo | ConvertTo-Json | Out-File $versionJsonFilePath -Encoding utf8
+Start-Git "add `"$versionJsonFilePath`""
+Start-Git "commit -m `"Set version to $newVersion`""
+
+Write-Host "Version(s) updated successfully"
