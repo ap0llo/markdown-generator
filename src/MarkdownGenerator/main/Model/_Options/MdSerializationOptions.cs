0cbc51921c0f86519472c06a6a1fd2ec861c7262
Andreas Grünwald <andreasgruenwald@live.de>
2018-08-01 22:12:27 +0200

    Add MaxLineLength option

--- src/MarkdownGenerator/main/Model/_Options/MdSerializationOptions.cs
+++ src/MarkdownGenerator/main/Model/_Options/MdSerializationOptions.cs
@@ -31,5 +31,19 @@
         /// Gets or sets the style for ordered list items
         /// </summary>
         public MdOrderedListStyle OrderedListStyle { get; set; } = MdOrderedListStyle.Dot;
+
+        /// <summary>
+        /// The maximum length of a line in the output.
+        /// When set to a value greater than 0, linebreaks
+        /// will be inserted after the specified number of characters
+        /// when possible.        
+        /// </summary>
+        /// <remarks>
+        /// Not all types of blocks can be split into multiple lines. 
+        /// Also line breaks will only be inserted between words, so if 
+        /// the length of a word exceeds the maximum line length the max length
+        /// cannot be adhered to
+        /// </remarks>
+        public int MaxLineLength { get; set; } = -1;
     }
 }
