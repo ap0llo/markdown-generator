71b97341d7bf25604f63c625765c1ccd47b32ece
Andreas Gruenwald <andreasgruenwald@live.de>
2019-03-11 11:50:44 +0100

    Add SyntaxVisualizer that can be used to write the syntax tree to a string

---
+++ src/MarkdownGenerator.Test/Internal/SyntaxVisualizerTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grynwald.MarkdownGenerator.Internal;
+using Xunit;
+
+namespace Grynwald.MarkdownGenerator.Test.Internal
+{
+    public class SyntaxVisualizerTest
+    {
+        [Fact]
+        public void GetSyntaxTree_returns_expected_result_01() =>
+            AssertSyntaxTreeEquals(
+                "MdContainerBlock\r\n",
+                new MdContainerBlock()
+            );
+
+        [Fact]
+        public void GetSyntaxTree_returns_expected_result_02() =>
+            AssertSyntaxTreeEquals(
+                "MdContainerBlock\r\n" +
+                "  MdParagraph\r\n",
+                new MdContainerBlock(new MdParagraph())
+            );
+
+        [Fact]
+        public void GetSyntaxTree_returns_expected_result_03() =>
+            AssertSyntaxTreeEquals(
+                "MdContainerBlock\r\n" +
+                "  MdParagraph\r\n" +
+                "  MdParagraph\r\n",
+                new MdContainerBlock(new MdParagraph(), new MdParagraph())
+            );
+
+        [Fact]
+        public void GetSyntaxTree_returns_expected_result_04() =>
+            AssertSyntaxTreeEquals(
+                "MdBulletList\r\n" +
+                "  MdListItem\r\n" +
+                "    MdParagraph\r\n" +
+                "  MdListItem\r\n",
+                new MdBulletList(new MdListItem(new MdParagraph()), new MdListItem())
+            );
+
+        [Fact]
+        public void GetSyntaxTree_returns_expected_result_05() =>
+            AssertSyntaxTreeEquals(
+                "MdOrderedList\r\n" +
+                "  MdListItem\r\n" +
+                "    MdParagraph\r\n" +
+                "  MdListItem\r\n",
+                new MdOrderedList(new MdListItem(new MdParagraph()), new MdListItem())
+            );
+
+
+        [Fact]
+        public void GetSyntaxTree_returns_expected_result_06() =>
+            AssertSyntaxTreeEquals(
+                "MdTable\r\n" +
+                "  MdTableRow\r\n" +
+                "  MdTableRow\r\n",
+                new MdTable(new MdTableRow(), new MdTableRow())
+            );
+
+
+        [Fact]
+        public void GetSyntaxTree_returns_expected_result_07() =>
+            AssertSyntaxTreeEquals(
+                "MdBlockQuote\r\n" +
+                "  MdParagraph\r\n" +
+                "  MdParagraph\r\n",
+                new MdBlockQuote(new MdParagraph(), new MdParagraph())
+            );
+
+
+        private void AssertSyntaxTreeEquals(string expected, MdBlock block)
+        {
+            var actual = SyntaxVisualizer.GetSyntaxTree(block);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/MarkdownGenerator/Internal/SyntaxVisualizer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+
+namespace Grynwald.MarkdownGenerator.Internal
+{
+    internal class SyntaxVisualizer
+    {
+        private class SyntaxVisualizerVisitor : IBlockVisitor
+        {
+            private readonly StringBuilder m_Builder;
+            private int m_Indentation = 0;
+
+
+            public SyntaxVisualizerVisitor(StringBuilder writer)
+            {
+                m_Builder = writer ?? throw new ArgumentNullException(nameof(writer));
+            }
+
+
+            public void Visit(MdHeading heading) => AppendTypeName(heading);
+
+            public void Visit(MdParagraph paragraph) => AppendTypeName(paragraph);
+
+            public void Visit(MdContainerBlock containerBlock) => VisitContainer(containerBlock);
+
+            public void Visit(MdThematicBreak thematicBreak) => AppendTypeName(thematicBreak);
+
+            public void Visit(MdTable table)
+            {
+                AppendTypeName(table);
+
+                m_Indentation += 1;
+
+                AppendTypeName(table.HeaderRow);
+                foreach (var row in table.Rows)
+                {
+                    AppendTypeName(row);
+                }
+
+                m_Indentation -= 1;
+            }
+
+            public void Visit(MdBulletList bulletList) => VisitList(bulletList);
+
+            public void Visit(MdOrderedList orderedList) => VisitList(orderedList);
+
+            public void Visit(MdListItem listItem) => VisitContainer(listItem);
+
+            public void Visit(MdEmptyBlock emptyBlock) => AppendTypeName(emptyBlock);
+
+            public void Visit(MdBlockQuote blockQuote) => VisitContainer(blockQuote);
+
+            public void Visit(MdCodeBlock codeBlock) => AppendTypeName(codeBlock);
+
+
+            private void AppendTypeName(object item)
+            {
+                for (int i = 0; i < m_Indentation; i++)
+                {
+                    m_Builder.Append("  ");
+                }
+                m_Builder.AppendLine(item.GetType().Name);
+            }
+
+            public void VisitContainer(MdContainerBlockBase block)
+            {
+                AppendTypeName(block);
+
+                m_Indentation += 1;
+                foreach (var item in block)
+                {
+                    item.Accept(this);
+                }
+                m_Indentation -= 1;
+            }
+
+            private void VisitList(MdList list)
+            {
+                AppendTypeName(list);
+
+                m_Indentation += 1;
+                foreach (var listItem in list)
+                {
+                    listItem.Accept(this);
+                }
+                m_Indentation -= 1;
+            }
+        }
+
+
+        public static string GetSyntaxTree(MdBlock block)
+        {
+            var stringBuilder = new StringBuilder();
+            var visitor = new SyntaxVisualizerVisitor(stringBuilder);
+            block.Accept(visitor);
+            return stringBuilder.ToString();
+        }
+
+    }
+}
