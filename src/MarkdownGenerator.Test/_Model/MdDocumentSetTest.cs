4286f3a0f2390d56630b1149cd7a7568414c77e9
Andreas Gruenwald <andreasgruenwald@live.de>
2019-06-15 23:39:09 +0200

    Add MdDocumentSet
    MdDocumentSet is a collection of documents associated with a (relative) path that can be saved to a directory together

---
+++ src/MarkdownGenerator.Test/_Model/MdDocumentSetTest.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.IO;
+using Grynwald.Utilities.IO;
+using Xunit;
+
+namespace Grynwald.MarkdownGenerator.Test
+{
+    public class MdDocumentSetTest
+    {
+        [Fact]
+        public void Indexer_returns_expected_document()
+        {
+            var path = "some/path/doc.md";
+            var set = new MdDocumentSet();
+            var document = set.CreateDocument(path);
+
+            Assert.Same(document, set[path]);
+        }
+
+
+        [Fact]
+        public void Documents_is_initially_empty()
+        {
+            var set = new MdDocumentSet();
+
+            Assert.NotNull(set.Documents);
+            Assert.Empty(set.Documents);
+        }
+
+        [Fact]
+        public void Documents_returns_expected_documents()
+        {
+            var set = new MdDocumentSet();
+
+            var document1 = set.CreateDocument("path1");
+            var document2 = set.CreateDocument("path2");
+            var document3 = set.CreateDocument("path3");
+
+            Assert.Equal(3, set.Documents.Count);
+            Assert.Contains(document1, set.Documents);
+            Assert.Contains(document2, set.Documents);
+            Assert.Contains(document3, set.Documents);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void CreateDocument_throws_ArgumentException_for_empty_path(string path)
+        {
+            var set = new MdDocumentSet();
+            Assert.Throws<ArgumentException>(() => set.CreateDocument(path));
+        }
+
+        [Theory]
+        [InlineData("myPath.md", "myPath.md")]
+        [InlineData("some/path/myPath.md", "some/path/myPath.md")]
+        [InlineData("some/path/myPath.md", "some\\path\\myPath.md")]
+        [InlineData("some/path/../../myPath.md", "myPath.md")]
+        public void CreateDocument_throw_ArgumentException_for_duplicate_paths(string path1, string path2)
+        {
+            var set = new MdDocumentSet();
+
+            _ = set.CreateDocument(path1);
+            Assert.Throws<ArgumentException>(() => set.CreateDocument(path2));
+        }
+
+        [Theory]
+        [InlineData("../some/path/document.md")]
+        [InlineData("../../some/path/document.md")]
+        [InlineData("some/path/../../../document.md")]
+        [InlineData("/document.md")]
+        [InlineData(@"C:\document.md")]
+        public void CreateDocument_throw_ArgumentException_for_invalid_paths(string path)
+        {
+            var set = new MdDocumentSet();            
+            Assert.Throws<ArgumentException>(() => set.CreateDocument(path));
+        }
+
+        [Theory]
+        [InlineData("some/path/document.md")]
+        [InlineData("some/path/../document.md")]
+        public void CreateDocument_creates_a_new_Document_for_valid_paths(string path)
+        {
+            var set = new MdDocumentSet();
+            var document = set.CreateDocument(path);
+            Assert.NotNull(document);
+        }
+
+        [Theory]
+        [InlineData("some/path/document.md", "some/path/document.md")]
+        [InlineData(@"some\path\document.md", "some/path/document.md")]
+        [InlineData(@"some\path/document.md", "some/path/document.md")]
+        [InlineData("some/path/../document.md", "some/document.md")]
+        [InlineData(@"some\path\..\document.md", "some/document.md")]
+        [InlineData("some/path/./document.md", "some/path/document.md")]
+        [InlineData(@"some\path\.\document.md", "some/path/document.md")]
+        [InlineData("some/path/../path/document.md", "some/path/document.md")]
+        [InlineData(@"some\path\..\path\document.md", "some/path/document.md")]
+        public void CreateDocument_normalizes_paths(string path, string expectedNormalizedPath)
+        {
+            var set = new MdDocumentSet();
+            var document = set.CreateDocument(path);
+
+            var actualNormalizedPath = set.GetPath(document);
+
+            Assert.Equal(expectedNormalizedPath, actualNormalizedPath);
+        }
+
+
+        [Fact]
+        public void Add_throws_ArgumentException_is_document_is_adde_twice()
+        {
+            var set = new MdDocumentSet();
+            var document = new MdDocument();
+
+            set.Add("doc1.md", document);
+
+            Assert.Throws<ArgumentException>(() => set.Add("doc2.md", document));
+        }
+
+
+        [Fact]
+        public void GetPath_returns_a_documents_path()
+        {
+            var path = "some/path/doc.md";
+            var set = new MdDocumentSet();
+            var document = set.CreateDocument(path);
+
+            Assert.Equal(path, set.GetPath(document));
+        }
+
+
+        [Fact]
+        public void Save_saves_all_documents()
+        {
+            using(var directory = new TemporaryDirectory())
+            {
+                // ARRANGE
+                var set = new MdDocumentSet();
+                var document1 = set.CreateDocument("doc1.md");
+                var document2 = set.CreateDocument("subDir/doc2.md");
+
+                var expectedOutPath1 = Path.Combine(directory, "doc1.md");
+                var expectedOutPath2 = Path.Combine(directory, "subDir/doc2.md");
+
+                document1.Root.Add(new MdHeading(1, "Document 1"));
+                document2.Root.Add(new MdHeading(1, "Document 2"));
+
+                // ACT
+                set.Save(directory);
+
+                // ASSERT
+                Assert.True(File.Exists(expectedOutPath1));
+                Assert.True(File.Exists(expectedOutPath2));
+
+                Assert.Equal(document1.ToString(), File.ReadAllText(expectedOutPath1));
+                Assert.Equal(document2.ToString(), File.ReadAllText(expectedOutPath2));
+            }
+        }
+
+        [Fact]
+        public void Save_deletes_output_directory_if_cleanOutputDirectory_is_true()
+        {
+            using (var directory = new TemporaryDirectory())
+            {
+                // ARRANGE
+                var existingPath1 = Path.Combine(directory, "file1.txt");
+                var existingPath2 = Path.Combine(directory, "subDir/file2.txt");
+
+                Directory.CreateDirectory(Path.GetDirectoryName(existingPath1));
+                File.WriteAllText(existingPath1, "Content");
+                Directory.CreateDirectory(Path.GetDirectoryName(existingPath2));
+                File.WriteAllText(existingPath2, "Content");
+
+                var set = new MdDocumentSet();
+                var document1 = set.CreateDocument("doc1.md");
+                var document2 = set.CreateDocument("subDir/doc2.md");
+
+                var expectedOutPath1 = Path.Combine(directory, "doc1.md");
+                var expectedOutPath2 = Path.Combine(directory, "subDir/doc2.md");
+
+                
+                // ACT
+                set.Save(directory, cleanOutputDirectory: true);
+
+                // ASSERT
+                Assert.True(File.Exists(expectedOutPath1));
+                Assert.True(File.Exists(expectedOutPath2));
+                Assert.False(File.Exists(existingPath1));
+                Assert.False(File.Exists(existingPath2));
+            }
+        }
+
+        [Fact]
+        public void Save_keeps_redundant_files_in_output_directory_if_cleanOutputDirectory_is_false()
+        {
+            using (var directory = new TemporaryDirectory())
+            {
+                // ARRANGE
+                var existingPath1 = Path.Combine(directory, "file1.txt");
+                var existingPath2 = Path.Combine(directory, "subDir/file1.txt");
+
+                Directory.CreateDirectory(Path.GetDirectoryName(existingPath1));
+                File.WriteAllTextAsync(existingPath1, "Content");
+                Directory.CreateDirectory(Path.GetDirectoryName(existingPath2));
+                File.WriteAllTextAsync(existingPath2, "Content");
+
+                var set = new MdDocumentSet();
+                var document1 = set.CreateDocument("doc1.md");
+                var document2 = set.CreateDocument("subDir/doc2.md");
+
+                var expectedOutPath1 = Path.Combine(directory, "doc1.md");
+                var expectedOutPath2 = Path.Combine(directory, "subDir/doc2.md");
+
+
+                // ACT
+                set.Save(directory, cleanOutputDirectory: false);
+
+                // ASSERT
+                Assert.True(File.Exists(expectedOutPath1));
+                Assert.True(File.Exists(expectedOutPath2));
+                Assert.True(File.Exists(existingPath1));
+                Assert.True(File.Exists(existingPath2));
+            }
+        }
+    }
+}
---
+++ src/MarkdownGenerator/_Model/MdDocumentSet.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using Grynwald.Utilities.Collections;
+
+namespace Grynwald.MarkdownGenerator
+{
+    public sealed class MdDocumentSet
+    {
+        private readonly Dictionary<string, MdDocument> m_DocumentsByPath;
+        private readonly Dictionary<MdDocument, string> m_PathsByDocument;
+
+
+        public MdDocument this[string path] => m_DocumentsByPath[path ?? throw new ArgumentNullException(nameof(path))];
+
+        public IReadOnlyCollection<MdDocument> Documents { get; }
+
+
+        public MdDocumentSet()
+        {
+            m_DocumentsByPath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? new Dictionary<string, MdDocument>(StringComparer.OrdinalIgnoreCase)
+                : new Dictionary<string, MdDocument>(StringComparer.Ordinal);
+            m_PathsByDocument = new Dictionary<MdDocument, string>();
+            
+            Documents = ReadOnlyCollectionAdapter.Create(m_DocumentsByPath.Values);
+        }
+
+        
+        public MdDocument CreateDocument(string path)
+        {
+            var document = new MdDocument();
+            Add(path, document);
+            return document;
+        }
+
+        public void Add(string path, MdDocument document)
+        {
+            if (String.IsNullOrEmpty(path))
+                throw new ArgumentException("Value must not be null of empty.", nameof(path));
+
+            path = NormalizeRelativePath(path);
+
+            if (m_DocumentsByPath.ContainsKey(path))
+                throw new ArgumentException($"A document with path '{path}' already exists.", nameof(path));
+
+            if (m_PathsByDocument.ContainsKey(document))
+                throw new ArgumentException("The document has already been added.");
+
+            m_DocumentsByPath.Add(path, document);
+            m_PathsByDocument.Add(document, path);
+        }
+
+        public string GetPath(MdDocument document)
+        {
+            if (document == null)
+                throw new ArgumentNullException(nameof(document));
+
+            return m_PathsByDocument[document];
+        }
+
+        public void Save(string directoryPath) =>
+            Save(directoryPath, cleanOutputDirectory: false, serializationOptions: null);
+
+        public void Save(string directoryPath, bool cleanOutputDirectory) =>
+            Save(directoryPath, cleanOutputDirectory, serializationOptions: null);
+
+        public void Save(string directoryPath, bool cleanOutputDirectory, MdSerializationOptions serializationOptions)
+        {
+            if (directoryPath == null)
+                throw new ArgumentNullException(nameof(directoryPath));
+
+            // serializationOptions can be null
+
+            if(Directory.Exists(directoryPath) && cleanOutputDirectory)
+            {
+                Directory.Delete(directoryPath, recursive: true);
+            }
+
+            foreach (var keyValuePair in m_DocumentsByPath)
+            {
+                var path = keyValuePair.Key;
+                var document = keyValuePair.Value;
+
+                var outPath = Path.Combine(directoryPath, path);
+
+                Directory.CreateDirectory(Path.GetDirectoryName(outPath));
+                document.Save(outPath, serializationOptions);
+            }
+        }
+
+
+        private string NormalizeRelativePath(string value)
+        {
+            if(Path.IsPathRooted(value))
+                throw new ArgumentException($"Path '{value}' is not a relative path", nameof(value));
+
+            // normalize directory separators to always use '/'
+            value = value.Replace(Path.DirectorySeparatorChar, '/').Replace(Path.AltDirectorySeparatorChar, '/');
+
+            // convert path into absolute path to remove any navigation elements (e.g. '..')
+
+            // use the current directory as root path (any valid path will do)
+            var rootPath = Environment.CurrentDirectory;
+            if (!rootPath.EndsWith("/"))
+                rootPath += "/";
+            var rootPathUri = new Uri(rootPath);
+
+            var fullPath = Path.Combine(rootPath, value);
+            var fullPathUri = new Uri(fullPath);
+
+            // ensure the relative path does not refer to a location outside the root directory
+            if (!fullPathUri.AbsolutePath.StartsWith(rootPathUri.AbsolutePath))
+                throw new ArgumentException($"Relative path '{value}' leaves the root directory");
+
+            // return a (normalized) relative path
+            return rootPathUri.MakeRelativeUri(fullPathUri).ToString();                       
+        }
+    }
+}
