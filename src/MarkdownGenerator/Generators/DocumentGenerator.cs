6902edf0df13d6abf88cfd8fc8ac577ca138b06a
Andreas Gruenwald <andreas@grynwald.net>
2019-06-16 22:23:30 +0200

    Add (mostly unimplemented) DocumentGenerator class to explore possible API shape

---
+++ src/MarkdownGenerator/Generators/DocumentGenerator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Grynwald.MarkdownGenerator.Generators
+{
+    internal class EqualityComparerAdapter<T> : IEqualityComparer<object>
+    {
+        private readonly IEqualityComparer<T> m_InnerComparer;
+
+        public EqualityComparerAdapter(IEqualityComparer<T> innerComparer)
+        {
+            m_InnerComparer = innerComparer;
+        }
+
+        public new bool Equals(object x, object y) => m_InnerComparer.Equals((T)x, (T)y);
+
+        public int GetHashCode(object obj) => m_InnerComparer.GetHashCode((T)obj);
+    }
+
+
+
+    public class DocumentGenerator
+    {
+        private readonly Dictionary<Type, Action<object, DocumentGenerator>> m_Handlers = new Dictionary<Type, Action<object, DocumentGenerator>>();       
+        private readonly Dictionary<Type, Dictionary<object, MdDocument>> m_Documents;
+
+        public MdDocumentSet DocumentSet { get; }
+
+
+        public DocumentGenerator() : this(new MdDocumentSet())
+        { }
+
+        public DocumentGenerator(MdDocumentSet documentSet)
+        {
+            DocumentSet = documentSet ?? throw new ArgumentNullException(nameof(documentSet));
+        }
+
+
+        public void AddHandler<T>(Action<T, DocumentGenerator> hander) => AddHandler(hander, EqualityComparer<T>.Default);
+        
+        public void AddHandler<T>(Action<T, DocumentGenerator> handler, IEqualityComparer<T> itemComparer)
+        {
+            //TODO: Check if handler for T already exists
+
+            m_Handlers.Add(typeof(T), (object dataItem, DocumentGenerator mapper) => handler((T)dataItem, mapper));
+            m_Documents.Add(typeof(T), new Dictionary<object, MdDocument>(new EqualityComparerAdapter<T>(itemComparer)));
+        }
+
+
+        public bool ContainsDocument<T>(T item)
+        {
+            //TODO: if there is no exact match for T, search is T is subtype of any known type
+
+            if (!m_Documents.ContainsKey(typeof(T)))
+                return false;
+
+            return m_Documents[typeof(T)].ContainsKey(item);
+        }
+
+
+        public bool TryGetDocument<T>(T item, out MdDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public void RegisterDocument<T>(T item, string path, MdDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void AddItem<T>(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public MdSpan TryGetLink<T1, T2>(T1 from, T2 to, MdSpan linkText)
+        {
+            if(TryGetDocument(from, out var fromDoc) && TryGetDocument(to, out var toDoc))
+            {
+                return DocumentSet.GetLink(fromDoc, toDoc, linkText);
+            }
+            else
+            {
+                return linkText;
+            }
+        }
+
+
+    }
+}
---
+++ src/MarkdownGenerator/Generators/GeneratorsSample.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Grynwald.MarkdownGenerator.Generators
+{
+    class DocumentMapperSample
+    {
+
+        static void Example()
+        {
+            var generator = new DocumentGenerator();
+
+
+            generator.AddHandler((DirectoryInfo dir, DocumentGenerator context) =>
+            {
+                var fileList = new MdBulletList();
+
+                var document = new MdDocument(
+                    new MdHeading(1, $"Directory {dir.Name}"),
+                    new MdHeading(2, "Files"),
+                    fileList);
+
+                context.RegisterDocument(dir, $"dirs/{dir.Name}.md", document);
+
+
+                foreach (var file in dir.EnumerateFiles())
+                {
+                    context.AddItem(file);
+
+                    fileList.Add(new MdListItem(context.TryGetLink(dir, file, file.Name)));                    
+                }
+
+            });
+
+            generator.AddHandler((FileInfo file, DocumentGenerator context) =>
+            {
+                var document = new MdDocument(
+                    new MdHeading(1, $"File {file.Name}"),
+                    new MdParagraph("Last Write Time: " + file.LastWriteTime.ToString())
+                );
+
+                context.RegisterDocument(file, $"files/{file.Name}.md", document);
+
+                var parentDirectoryInfo = new DirectoryInfo(file.DirectoryName);
+
+                document.Root.Add(
+                    new MdParagraph("Parent directory: ", context.TryGetLink(file, parentDirectoryInfo, parentDirectoryInfo.Name))
+                );
+            });
+
+
+            foreach(var dir in new DirectoryInfo("C:\\Example").EnumerateDirectories())
+            {
+                generator.AddItem(dir);
+            }
+
+        }
+
+    }
+}
