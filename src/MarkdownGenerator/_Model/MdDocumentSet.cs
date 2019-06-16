49248086badddd93a407939b39247b1526a2410e
Andreas Gruenwald <andreas@grynwald.net>
2019-06-16 20:04:28 +0200

    Add inline docs for MdDocumentSet

--- src/MarkdownGenerator/_Model/MdDocumentSet.cs
+++ src/MarkdownGenerator/_Model/MdDocumentSet.cs
@@ -3,16 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
-using Grynwald.Utilities.Collections;
 
 namespace Grynwald.MarkdownGenerator
-{    
+{
+    /// <summary>
+    /// A collection of documents associated with a path.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="MdDocumentSet"/> allows grouping of multiple <see cref="MdDocument"/> instances and associating
+    /// them with a path. The document set offers methods to create links between documents and saving all 
+    /// documents to a directory at once.
+    /// <para>
+    /// <see cref="MdDocumentSet"/> implements a 1:1 mapping between documents and paths.
+    /// </para>
+    /// </remarks>
     public sealed class MdDocumentSet : IEnumerable<MdDocument>
     {
         private readonly Dictionary<string, MdDocument> m_DocumentsByPath;
         private readonly Dictionary<MdDocument, string> m_PathsByDocument;
 
-
+        /// <summary>
+        /// Gets the document associated with the specified path.
+        /// </summary>
+        /// <param name="path">The path to get the document for.</param>
+        /// <returns>Returns the document associated with the specified path.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="path"/> is null.</exception>
+        /// <exception cref="DocumentNotFoundException">Thrown when no document associated with the specified path was found.</exception>
         public MdDocument this[string path]
         {
             get
@@ -27,6 +43,13 @@ namespace Grynwald.MarkdownGenerator
             }
         }
 
+        /// <summary>
+        /// Gets the path associated with the specified document.
+        /// </summary>
+        /// <param name="document">The document which's path to get.</param>
+        /// <returns>Returns the path for the specified document.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="document"/> is null.</exception>
+        /// <exception cref="DocumentNotFoundException">Thrown when the specified document is not part of this document set.</exception>
         public string this[MdDocument document]
         {
             get
@@ -41,30 +64,46 @@ namespace Grynwald.MarkdownGenerator
             }
         }
 
+        /// <summary>
+        /// Gets all the documents in this document set.
+        /// </summary>
+        public IEnumerable<MdDocument> Documents => m_DocumentsByPath.Values;
 
-        public IReadOnlyCollection<MdDocument> Documents { get; }
 
-
+        /// <summary>
+        /// Initializes a new instance of <see cref="MdDocumentSet"/>.
+        /// </summary>
         public MdDocumentSet()
         {
+            // treat paths case-insensitive on Windows
             m_DocumentsByPath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
                 ? new Dictionary<string, MdDocument>(StringComparer.OrdinalIgnoreCase)
                 : new Dictionary<string, MdDocument>(StringComparer.Ordinal);
             
-            m_PathsByDocument = new Dictionary<MdDocument, string>();
-            
-            Documents = ReadOnlyCollectionAdapter.Create(m_DocumentsByPath.Values);
+            m_PathsByDocument = new Dictionary<MdDocument, string>();            
         }
 
-
+        /// <summary>
+        /// Determines whether the specified document is part of the document set.
+        /// </summary>
         public bool ContainsDocument(MdDocument document) => m_PathsByDocument.ContainsKey(document);
 
+        /// <summary>
+        /// Determines whether a document with the specified path is part of the document set.
+        /// </summary>
         public bool ContainsPath(string path)
         {
             path = NormalizeRelativePath(path);
             return m_DocumentsByPath.ContainsKey(path);
         }
-        
+
+        /// <summary>
+        /// Creates a new <see cref="MdDocument"/> and adds it to the document set.
+        /// </summary>
+        /// <param name="path">The path of the document to create.</param>
+        /// <returns>Returns the created document.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="path"/> is null of empty.</exception>
+        /// <exception cref="ArgumentException">Thrown when the document set already contains a document with the specified path.</exception>
         public MdDocument CreateDocument(string path)
         {
             var document = new MdDocument();
@@ -72,10 +111,27 @@ namespace Grynwald.MarkdownGenerator
             return document;
         }
 
+        /// <summary>
+        /// Adds the specified document to the set at the specified path.
+        /// </summary>
+        /// <param name="path">The document's relative path.</param>
+        /// <param name="document">The document to add to the document set.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="path"/> is null of empty.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="document"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when the document set already contains a document with the specified path.</exception>
+        /// <exception cref="ArgumentException">Thrown when the document set already contains the specified document.</exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="path"/> is not a valid relative path.
+        /// Value must not be rooted and must not refer to a location outside the root output directory,
+        /// e.g. <c>../doc.md</c>.
+        /// </exception>
         public void Add(string path, MdDocument document)
         {
             if (String.IsNullOrEmpty(path))
                 throw new ArgumentException("Value must not be null of empty.", nameof(path));
+
+            if (document == null)
+                throw new ArgumentNullException(nameof(document));
 
             path = NormalizeRelativePath(path);
 
@@ -88,7 +144,19 @@ namespace Grynwald.MarkdownGenerator
             m_DocumentsByPath.Add(path, document);
             m_PathsByDocument.Add(document, path);
         }
-        
+
+        /// <summary>
+        /// Creates a markdown link element (see <see cref="MdLinkSpan"/>) with a relative link between the specified documents.
+        /// </summary>
+        /// <param name="from">The link source (i.e. the document the link span will be placed in).</param>
+        /// <param name="to">The links's target.</param>
+        /// <param name="linkText">The link text</param>
+        /// <returns>Returns a new <see cref="MdLinkSpan"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="from"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="to"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="linkText"/> is null.</exception>
+        /// <exception cref="DocumentNotFoundException">Thrown when <paramref name="from"/> is not part of the document set.</exception>
+        /// <exception cref="DocumentNotFoundException">Thrown when <paramref name="to"/> is not part of the document set.</exception>
         public MdLinkSpan GetLink(MdDocument from, MdDocument to, MdSpan linkText)
         {
             if (from == null)
@@ -110,16 +178,33 @@ namespace Grynwald.MarkdownGenerator
             return new MdLinkSpan(linkText, uri);
         }
 
+        /// <inheritdoc />
         public IEnumerator<MdDocument> GetEnumerator() => Documents.GetEnumerator();
 
+        /// <inheritdoc />
         IEnumerator IEnumerable.GetEnumerator() => Documents.GetEnumerator();
 
+        /// <summary>
+        /// Saves all the set's documents to the specified output directory.
+        /// </summary>
+        /// <param name="directoryPath">The directory path to save the documents to.</param>
         public void Save(string directoryPath) =>
             Save(directoryPath, cleanOutputDirectory: false, serializationOptions: null);
 
+        /// <summary>
+        /// Saves all the set's documents to the specified output directory.
+        /// </summary>
+        /// <param name="directoryPath">The directory path to save the documents to.</param>
+        /// <param name="cleanOutputDirectory">Determines whether the output directory is deleted before saving the documents.</param>
         public void Save(string directoryPath, bool cleanOutputDirectory) =>
             Save(directoryPath, cleanOutputDirectory, serializationOptions: null);
 
+        /// <summary>
+        /// Saves all the set's documents to the specified output directory.
+        /// </summary>
+        /// <param name="directoryPath">The directory path to save the documents to.</param>
+        /// <param name="cleanOutputDirectory">Determines whether the output directory is deleted before saving the documents.</param>
+        /// <param name="serializationOptions">The serialization options to use for saving the documents. Can be <c>null</c>.</param>
         public void Save(string directoryPath, bool cleanOutputDirectory, MdSerializationOptions serializationOptions)
         {
             if (directoryPath == null)
