046f221b155ab0e6c6c8fca9b22e6762676119d5
Andreas Gruenwald <andreas@grynwald.net>
2019-06-30 12:21:45 +0200

    Add test to ensure the public api is not changed accidentally.

---
+++ src/MarkdownGenerator.Test.ApiApproval/PublicApiApproval.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Grynwald.MarkdownGenerator;
+using PublicApiGenerator;
+using Shouldly;
+using Xunit;
+
+namespace MarkdownGenerator.Test.ApiApproval
+{
+    public class PublicApiApproval
+    {
+        [Fact]
+        public void MarkdownGenerator_must_not_have_unapproved_API_changes()
+        {
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(MdDocument).Assembly);
+            
+            publicApi.ShouldMatchApproved();
+        }
+
+    }
+}
