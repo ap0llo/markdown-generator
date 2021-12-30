a38fc6f8e6004847c76386ea2d7ff5730a2a6595
Andreas Grünwald <andreas@grynwald.net>
2021-12-30 15:54:42 +0100

    build(deps): Update dependency 'grynwald.changelog' to version 0.5.82-pre
    Downgrade referecne to use a stable version of the changelog tool
    
    Updates-Dependency: grynwald.changelog
    Updates-dependency-from-version: 0.5.82-pre
    Updates-dependency-to-version: 0.4.135

--- build.ps1
+++ build.ps1
@@ -1,5 +1,8 @@
 $ErrorActionPreference = "Stop"
 
+# Install .NET 5 runtime (requried for runnign some of the local tools during build)
+./build/dotnet-install.ps1 -Channel 5.0 -Runtime dotnet
+
 # Install SDK and runtime as specified in global.json
 ./build/dotnet-install.ps1 -JsonFile "$PSScriptRoot/global.json"
 
